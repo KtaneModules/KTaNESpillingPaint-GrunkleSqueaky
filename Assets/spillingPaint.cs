deb62dc0a1fe38fe210d14cbe1f49d9f1c142e6c
Grunkle Squeaky <grunklesqueaky@gmail.com>
2021-08-09 04:42:31 -0400

    Upload

---
+++ Assets/spillingPaint.cs
@@ -0,0 +1,223 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KModkit;
+
+public class spillingPaint : MonoBehaviour {
+
+    enum Colors {white,red,orange,yellow,green,blue,purple}
+    public KMBombInfo Bomb;
+    public KMAudio Audio;
+    public KMBombModule Module;
+    public KMSelectable[] fieldButtons;
+    public KMSelectable[] colorSelector;
+    public KMSelectable resetButton;
+    public Material[] colorMats;
+
+    static int moduleIdCounter = 1;
+    int moduleId;
+    private bool moduleSolved;
+
+    Colors selectedColor;
+    private List<Colors> placedColors = new List<Colors>();
+    private List<int> placedPositions = new List<int>();
+    int spillCount = 0;
+
+    int[,] field = new int[,]
+    {
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0}
+    };
+    int[,] puzzle = new int[,] 
+    {
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0},
+        {0,0,0,0,0,0}
+    };
+
+    void Awake ()
+    {
+        moduleId = moduleIdCounter++;
+        for (int i = 0; i < 36; i++)
+        {
+            int ix = i;
+            fieldButtons[ix].OnInteract += delegate () { FieldPress(ix); Audio.PlaySoundAtTransform("fieldSelectSound", fieldButtons[ix].transform); return false; };
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            int ix = i;
+            colorSelector[ix].OnInteract += delegate () { SelectorPress(ix); return false; };
+        }
+        resetButton.OnInteract += delegate () { ResetPress(); return false; };
+    }
+
+    void FieldPress (int index)
+    {
+        spillCount++;
+        int y = index / 6;
+        int x = index % 6;
+        for (int y1 = -1; y1 < 2; y1++)
+        {
+            for (int x1 = -1; x1 < 2; x1++)
+            {
+                if((y+y1 < 0) || (y+y1 > 5) || (x+x1 < 0) || (x+x1 > 5)) { continue; }
+                FieldColor(y + y1, x + x1);
+            }
+        }
+        if (spillCount == 7)
+        {
+            CheckSolve();
+        }
+    }
+
+    void FieldColor (int y, int x)
+    {
+        field[y, x] += (int)selectedColor;
+        field[y, x] %= 7;
+        fieldButtons[y * 6 + x].GetComponent<MeshRenderer>().material = colorMats[field[y, x]];
+    }
+
+    void CheckSolve ()
+    {
+        for (int i = 0; i < 36; i++)
+        { 
+            if (field[i/6,i%6] != 0)
+            {
+                ResetPress();
+                return;
+            }
+        }
+        Module.HandlePass();
+    }
+
+    void SelectorPress (int index)
+    {
+        colorSelector[index].AddInteractionPunch(0.25f);
+        if ((int)selectedColor != index + 1)
+        { Audio.PlaySoundAtTransform("colorSelectorSelectSound", colorSelector[index].transform); }
+        selectedColor = (Colors)(index + 1);
+        resetButton.GetComponent<MeshRenderer>().material = colorMats[index + 1];
+    }
+
+    void ResetPress ()
+    {
+        Audio.PlaySoundAtTransform("resetSelectSound", resetButton.transform);
+        resetButton.AddInteractionPunch(0.5f);
+        spillCount = 0;
+        for (int i = 0; i < 36; i++)
+        {
+            field[i / 6, i % 6] = puzzle[i / 6, i % 6];
+            fieldButtons[i].GetComponent<MeshRenderer>().material = colorMats[field[i / 6, i % 6]];
+        }
+    }
+
+    void Start ()
+    {
+        int[] chosenPositions = Enumerable.Range(0, 36).ToArray().Shuffle().Take(7).ToArray();
+        foreach (int pos in chosenPositions)
+        {
+            selectedColor = (Colors)UnityEngine.Random.Range(1, 7);
+            placedColors.Add(selectedColor);
+            placedPositions.Add(pos);
+            FieldPress(pos);
+            Debug.LogFormat("[Spilling Colors #{0}] Spilled {1} at position {2} in reading order.", moduleId, selectedColor.ToString(), pos + 1);
+            spillCount = 0;
+        }
+        for (int i = 0; i < 36; i++)
+        {
+            puzzle[i / 6, i % 6] = field[i / 6, i % 6];
+        }
+        resetButton.GetComponent<MeshRenderer>().material = colorMats[0];
+    }
+
+    #pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"Use <!{0} red> to change colors (red/orange/yellow/green/blue/purple), <!{0} A1> to spill the selected color in that cell (A1-F6, letters for columns), and <!{0} reset> to reset. Commands can be chained with semicolons, for example <!{0} red;A1;A2;blue;D3>";
+    #pragma warning restore 414
+
+    IEnumerator ProcessTwitchCommand (string command)
+    {
+        command = command.Trim().ToLowerInvariant();
+        List<string> parameters = command.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        yield return null;
+        if (command == "reset")
+        {
+            resetButton.OnInteract();
+        }
+        else
+        {
+            for (int i = 0; i < parameters.Count(); i++)
+            {
+                if (parameters[i] == "red")
+                {
+                    colorSelector[0].OnInteract();
+                }
+                else if (parameters[i] == "orange")
+                {
+                    colorSelector[1].OnInteract();
+                }
+                else if (parameters[i] == "yellow")
+                {
+                    colorSelector[2].OnInteract();
+                }
+                else if (parameters[i] == "green")
+                {
+                    colorSelector[3].OnInteract();
+                }
+                else if (parameters[i] == "blue")
+                {
+                    colorSelector[4].OnInteract();
+                }
+                else if (parameters[i] == "purple")
+                {
+                    colorSelector[5].OnInteract();
+                }
+                else if (!"abcdef".Contains(parameters[i][0]) || !"123456".Contains(parameters[i][1]))
+                {
+                    yield return "sendtochaterror I don't understand!";
+                }
+                else
+                {
+                    int Index = 0;
+                    for (int r = 0; r < 6; r++)
+                    {
+                        if (parameters[i][1] == "123456"[r])
+                        {
+                            Index += r * 6;
+                        }
+                    }
+                    for (int r = 0; r < 6; r++)
+                    {
+                        if (parameters[i][0] == "abcdef"[r])
+                        {
+                            Index += r;
+                        }
+                    }
+                    fieldButtons[Index].OnInteract();
+                }
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+
+    IEnumerator TwitchHandleForcedSolve ()
+    {
+        yield return null;
+        for (int i = 0; i < 7; i++)
+        {
+            colorSelector[(7 - (int)placedColors[i]) - 1].OnInteract();
+            yield return new WaitForSeconds(0.2f);
+            fieldButtons[placedPositions[i]].OnInteract();
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
